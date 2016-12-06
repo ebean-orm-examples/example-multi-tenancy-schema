e5ca0967d1347c1f7646f35885f9962a4d2c9898
Peace <orepeace@gmail.com>
2016-12-06 07:53:34 +0100

    lazy loading

--- src/test/java/org/example/domain/ContentTest.java
+++ src/test/java/org/example/domain/ContentTest.java
@@ -4,59 +4,75 @@ import com.avaje.ebean.Ebean;
 import org.example.bootstrap.UserContext;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
 public class ContentTest extends BaseTestCase {
 
-  @Test
-  public void insert() {
+	@Test
+	public void insert() {
 
-    UserContext.set("rob", "ten_1");
+		UserContext.set("rob", "ten_1");
 
-    Content bean = new Content();
-    bean.setTitle("Testing 123");
-    bean.setByline("Thinking about testing with databases");
-    bean.setBody("Lots of interesting stuff to say here");
-    bean.save();
+		Author author = new Author();
+		author.setName("rob");
+		Content bean = new Content();
+		bean.setTitle("Testing 123");
+		bean.setByline("Thinking about testing with databases");
+		bean.setBody("Lots of interesting stuff to say here");
+		author.setContents(Arrays.asList(bean));
+		author.save();
 
-    UserContext.set("roger", "ten_1");
+		UserContext.set("roger", "ten_1");
 
-    bean = new Content();
-    bean.setTitle("Testing integration");
-    bean.setByline("More testing databases");
-    bean.setBody("Meh");
-    bean.save();
+		author = new Author();
+		author.setName("roger");
+		bean = new Content();
+		bean.setTitle("Testing integration");
+		bean.setByline("More testing databases");
+		bean.setBody("Meh");
+		author.setContents(Arrays.asList(bean));
+		author.save();
 
+		UserContext.set("fi", "ten_2");
 
-    UserContext.set("fi", "ten_2");
+		Author OtherAuthor = new Author();
+		OtherAuthor.setName("fi");
+		Content beanOther = new Content();
+		beanOther.setTitle("Banana");
+		beanOther.setByline("Yummy and yellow");
+		beanOther.setBody("Food content");
+		OtherAuthor.setContents(Arrays.asList(beanOther));
+		OtherAuthor.save();
 
-    Content beanOther = new Content();
-    beanOther.setTitle("Banana");
-    beanOther.setByline("Yummy and yellow");
-    beanOther.setBody("Food content");
-    beanOther.save();
+		UserContext.set("some", "ten_1");
+		System.out.println("Tenant r1");
+		findAll();
 
+		UserContext.set("other", "ten_2");
+		System.out.println("Tenant f1");
+		findAll();
 
-    UserContext.set("some", "ten_1");
-    System.out.println("Tenant r1");
-    findAll();
+	}
 
-    UserContext.set("other", "ten_2");
-    System.out.println("Tenant f1");
-    findAll();
+	private void findAll() {
 
-  }
+		CompletionStage<List<Content>> allForTenant = CompletableFuture.supplyAsync(() -> {
+			UserContext.set("other", "ten_2");
+			System.out.println("find all (for the current tenant) " + UserContext.get().getTenantId());
+			return Ebean.find(Content.class).findList();
+		});
 
-  private void findAll() {
-
-    System.out.println("find all (for the current tenant) " + UserContext.get().getTenantId());
-
-    List<Content> allForTenant =
-        Ebean.find(Content.class)
-            .findList();
-
-    for (Content content : allForTenant) {
-      System.out.println(content);
-    }
-  }
+		try {
+			for (Content content : allForTenant.toCompletableFuture().get()) {
+				
+				System.out.println(content.getAuthor().name);
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
