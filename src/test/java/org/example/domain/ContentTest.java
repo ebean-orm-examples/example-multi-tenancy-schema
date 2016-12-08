fd914b002ccd766dc914ddc297aeb986811481b2
Rob Bygrave <robin.bygrave@gmail.com>
2016-12-08 22:07:54 +1300

    format only - whitespace

--- src/test/java/org/example/domain/ContentTest.java
+++ src/test/java/org/example/domain/ContentTest.java
@@ -12,10 +12,10 @@ import java.util.concurrent.ExecutionException;
 
 public class ContentTest extends BaseTestCase {
 
-	@Test
-	public void insert() {
+  @Test
+  public void insert() {
 
-		UserContext.set("rob", "ten_1");
+    UserContext.set("rob", "ten_1");
 
     Author author = new Author();
     author.setName("rob");
@@ -54,54 +54,54 @@ public class ContentTest extends BaseTestCase {
     System.out.println("Tenant f1");
     findAll1("ten_2");
 
-		System.out.println("--------------------------next round----------------------------\n");
+    System.out.println("--------------------------next round----------------------------\n");
 
-		System.out.println("Tenant One -----------");
-		findAll2("ten_1");
+    System.out.println("Tenant One -----------");
+    findAll2("ten_1");
 
-		System.out.println("Tenant Two -----------");
-		findAll1("ten_2");
+    System.out.println("Tenant Two -----------");
+    findAll1("ten_2");
 
-	}
+  }
 
-	//runs well
-	private void findAll1(String tenant) {
+  //runs well
+  private void findAll1(String tenant) {
 
-		CompletionStage<List<Content>> allForTenant = CompletableFuture.supplyAsync(() -> {
-			UserContext.set("other", tenant);
-			System.out.println("find all (for the current tenant) " + UserContext.get().getTenantId());
-			return Ebean.find(Content.class).findList();
-		}).thenApplyAsync(allContents -> {
-			allContents.forEach(content -> {
-				System.out.println("Author: " + content.getAuthor().getName());
-				System.out.println("Content: " + content);
-			});
-			return allContents;
-		});
-		try {
-			allForTenant.toCompletableFuture().get();
-		} catch (InterruptedException | ExecutionException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    CompletionStage<List<Content>> allForTenant = CompletableFuture.supplyAsync(() -> {
+      UserContext.set("other", tenant);
+      System.out.println("find all (for the current tenant) " + UserContext.get().getTenantId());
+      return Ebean.find(Content.class).findList();
+    }).thenApplyAsync(allContents -> {
+      allContents.forEach(content -> {
+        System.out.println("Author: " + content.getAuthor().getName());
+        System.out.println("Content: " + content);
+      });
+      return allContents;
+    });
+    try {
+      allForTenant.toCompletableFuture().get();
+    } catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-	//Strange behaviour and eventually throws.
-	private void findAll2(String tenant) {
+  //Strange behaviour and eventually throws.
+  private void findAll2(String tenant) {
 
-		CompletionStage<List<Content>> allForTenant = CompletableFuture.supplyAsync(() -> {
-			UserContext.set("other", tenant);
-			System.out.println("find all (for the current tenant) " + UserContext.get().getTenantId());
-			return Ebean.find(Content.class).findList();
-		});
+    CompletionStage<List<Content>> allForTenant = CompletableFuture.supplyAsync(() -> {
+      UserContext.set("other", tenant);
+      System.out.println("find all (for the current tenant) " + UserContext.get().getTenantId());
+      return Ebean.find(Content.class).findList();
+    });
 
-		try {
-			allForTenant.toCompletableFuture().get().forEach(content -> {
-				// tenantId automatically provided by "load context"
-				System.out.println("Author: " + content.getAuthor().getName());
-				System.out.println("Content: " + content);
-			});
-		} catch (InterruptedException | ExecutionException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    try {
+      allForTenant.toCompletableFuture().get().forEach(content -> {
+        // tenantId automatically provided by "load context"
+        System.out.println("Author: " + content.getAuthor().getName());
+        System.out.println("Content: " + content);
+      });
+    } catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
